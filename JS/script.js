d7bdbb8403a364e3f225aa2eb43c1514ddad5ca6
shriram <shriramvenugopal123@gmail.com>
2018-08-09 20:07:14 +0530

    made navbar responsive

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems, options);
   });
 
+function toggleMenu(e) {
+    var menubar = document.getElementsByClassName('nav-details')[0];
+    menubar.classList.toggle('toggleMenu');
+}
+
+function dropDown(e) {
+    var toggle = document.getElementsByClassName('dropDown-content')[0];
+    toggle.classList.toggle(' dropDown-content--Show');
+}
